b33731c1f10bf9b943311eb6a42949ade1a8ab64
Deekshitha <deekshitha@robosoftin.com>
2016-11-10 12:04:47 +0530

    Added log on view load.

---
+++ GitDemoProject/NewFeature.h
@@ -0,0 +1,13 @@
+//
+//  NewFeature.h
+//  GitDemoProject
+//
+//  Created by Naveen krishnan on 10/11/16.
+//  Copyright © 2016 Robosoft Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewFeature : NSObject
+
+@end
---
+++ GitDemoProject/NewFeature.m
@@ -0,0 +1,13 @@
+//
+//  NewFeature.m
+//  GitDemoProject
+//
+//  Created by Naveen krishnan on 10/11/16.
+//  Copyright © 2016 Robosoft Technologies. All rights reserved.
+//
+
+#import "NewFeature.h"
+
+@implementation NewFeature
+
+@end
